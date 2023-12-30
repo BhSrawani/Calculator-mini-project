f7442c0245b60b67004b5f5749e7d67154431e94
BH. SRAWANI <116104169+BhSrawani@users.noreply.github.com>
2023-12-30 23:55:50 +0530

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,53 +1,102 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator {
 
-    public static void main(String[] args) {
+    private JFrame frame;
+    private JTextField textField;
+    private double num1, num2, result;
+    private char operator;
 
-        double num1, num2, result;
-        char operator;
+    public Calculator() {
+        frame = new JFrame("Simple Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+        frame.setLayout(new BorderLayout());
 
-        Scanner scanner = new Scanner(System.in);
+        textField = new JTextField();
+        frame.add(textField, BorderLayout.NORTH);
 
-        System.out.print("Enter first number: ");
-        num1 = scanner.nextDouble();
+        JPanel buttonPanel = new JPanel(new GridLayout(4, 4));
 
-        System.out.print("Enter second number: ");
-        num2 = scanner.nextDouble();
+        String[] buttonLabels = {
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "0", ".", "=", "+"
+        };
 
-        System.out.print("Enter an operator (+, -, *, /): ");
-        operator = scanner.next().charAt(0);
+        for (String label : buttonLabels) {
+            JButton button = new JButton(label);
+            button.addActionListener(new ButtonClickListener());
+            buttonPanel.add(button);
+        }
 
-        scanner.close();
+        frame.add(buttonPanel, BorderLayout.CENTER);
 
+        frame.setVisible(true);
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JButton source = (JButton) e.getSource();
+            String buttonText = source.getText();
+
+            if (buttonText.matches("[0-9.]")) {
+                textField.setText(textField.getText() + buttonText);
+            } else if (buttonText.matches("[/*\\-+]")) {
+                if (!textField.getText().isEmpty()) {
+                    num1 = Double.parseDouble(textField.getText());
+                    operator = buttonText.charAt(0);
+                    textField.setText("");
+                }
+            } else if (buttonText.equals("=")) {
+                if (!textField.getText().isEmpty()) {
+                    num2 = Double.parseDouble(textField.getText());
+                    performOperation();
+                    textField.setText(String.valueOf(result));
+                }
+            }
+        }
+    }
+
+    private void performOperation() {
         switch (operator) {
             case '+':
                 result = num1 + num2;
                 break;
-
             case '-':
                 result = num1 - num2;
                 break;
-
             case '*':
                 result = num1 * num2;
                 break;
-
             case '/':
                 if (num2 != 0) {
                     result = num1 / num2;
                 } else {
-                    System.out.println("Error! Division by zero is not allowed.");
-                    return;
+                    JOptionPane.showMessageDialog(frame, "Error! Division by zero is not allowed.", "Error", JOptionPane.ERROR_MESSAGE);
+                    clear();
                 }
                 break;
-
-            // operator doesn't match any case constant (+, -, *, /)
-            default:
-                System.out.println("Error! Invalid operator. Please enter correct operator.");
-                return;
         }
+    }
 
-        System.out.printf("%.1f %c %.1f = %.1f\n", num1, operator, num2, result);
+    private void clear() {
+        num1 = 0;
+        num2 = 0;
+        result = 0;
+        operator = '\0';
+        textField.setText("");
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Calculator();
+            }
+        });
     }
 }
